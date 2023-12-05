7dbfbbc1f39dcb055c2a27d366be07b315d2cc07
Rui Cruz <ruicrux@hotmail.com>
2023-12-05 22:45:34 +0000

    FIX Requests

--- SCLINICO/sclinico_api/src/consultations/routes.js
+++ SCLINICO/sclinico_api/src/consultations/routes.js
@@ -12,9 +12,9 @@ router.get('/especialidades', controller.Get_All_Specialities);
 router.get('/diagnosticos', controller.Get_All_Diagnosis);
 
 router.get('/consultas', controller.Get_All_Consultations);
-router.get('/consultas/table', controller.Get_All_Consultations_Table);
+router.get('/consultas/table', controller.Get_All_Consultations_for_DataTable);
 router.get('/consultas/utente/:hashed_id', controller.Get_Consultations_By_Utente);
-router.get('/consultas/utente/table/:hashed_id', controller.Get_Consultations_By_Utente_Table);
+router.get('/consultas/utente/table/:hashed_id', controller.Get_Consultations_By_Utente_for_DataTable);
 
 router.post('/consultas/adicionar', controller.Add_Consultation);
 
