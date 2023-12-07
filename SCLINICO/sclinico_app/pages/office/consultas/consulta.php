e32f3a0156a22d8d542014d5c3db0e56957bf692
João Correia <j.correia2213@gmail.com>
2023-12-07 00:50:23 +0000

    FIX API SCLINICO e VER CONSULTA
    FIX API SCLINICO e VER CONSULTA

--- SCLINICO/sclinico_app/pages/office/consultas/consulta.php
+++ SCLINICO/sclinico_app/pages/office/consultas/consulta.php
@@ -16,6 +16,15 @@ $patologias_list = $patologias["response"];
 $diagnosticos = $api->fetch("consultations/diagnosticos", null, null);
 $diagnosticos_list = $diagnosticos["response"];
 
+$medicacao = $api->fetch("consultations/medicamentos", null, null);
+$medicacao_list = $medicacao["response"];
+
+$exames = $api->fetch("consultations/exames", null, null);
+$exames_list = $exames["response"];
+
+$vacinas = $api->fetch("consultations/vacinas", null, null);
+$vacinas_list = $vacinas["response"];
+
 $page_name = "CareSys | SCLINICO - Consulta - " . $consultation_info["utente"];
 ?>
 
@@ -141,6 +150,11 @@ $page_name = "CareSys | SCLINICO - Consulta - " . $consultation_info["utente"];
                                             <textarea class="form-control form-control-lg form-control-solid" rows="4" name="tratamento" placeholder="Preencha com o Tratamento"></textarea>
                                         </div>
 
+                                        <div class="col-12">
+                                            <label class="col-lg-12 col-form-label fw-semibold fs-6">Progresso Utente</label>
+                                            <textarea class="form-control form-control-lg form-control-solid" rows="3" name="progresso" placeholder="Preencha com o Progresso do Utente"></textarea>
+                                        </div>
+
                                         <div class="col-6">
                                             <label class="col-lg-12 col-form-label fw-semibold fs-6">Diagnósticos</label>
                                             <select class="form-select form-select-solid" name="diagnostico" multiple data-control="select2" data-placeholder="Selecione o(s) Diagnóstico(s)">
@@ -160,16 +174,48 @@ $page_name = "CareSys | SCLINICO - Consulta - " . $consultation_info["utente"];
                                             </select>
                                         </div>
 
+                                        <div class="separator separator-dotted separator-content border-dark my-15"><span class="h1 w-25">Prescrições</span></div>
+
+                                        <div class="col-4">
+                                            <label class="col-lg-12 col-form-label fw-semibold fs-6">Medicação</label>
+                                            <select class="form-select form-select-solid" name="medicacao" multiple data-control="select2" data-placeholder="Selecione a(s) Medicação(s)">
+                                                <option></option>
+                                                <?php foreach ($medicacao_list as $key => $value) { ?>
+                                                    <option value="<?php echo $value["id_medicamento"] ?>"><?php echo $value["codigo"] . ' - ' . $value["nome"] ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+
+                                        <div class="col-4">
+                                            <label class="col-lg-12 col-form-label fw-semibold fs-6">Exames</label>
+                                            <select class="form-select form-select-solid" name="exames" multiple data-control="select2" data-placeholder="Selecione o(s) Exame(s)">
+                                                <option></option>
+                                                <?php foreach ($exames_list as $key => $value) { ?>
+                                                    <option value="<?php echo $value["id_exame"] ?>"><?php echo $value["codigo"] . ' - ' . $value["nome"] ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+
+                                        <div class="col-4">
+                                            <label class="col-lg-12 col-form-label fw-semibold fs-6">Vacinas</label>
+                                            <select class="form-select form-select-solid" name="vacinas" multiple data-control="select2" data-placeholder="Selecione a(s) Vacina(s)">
+                                                <option></option>
+                                                <?php foreach ($vacinas_list as $key => $value) { ?>
+                                                    <option value="<?php echo $value["id_vacina"] ?>"><?php echo $value["codigo"] . ' - ' . $value["nome"] ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+
                                         <div class="col-12 mt-6">
                                             <div class="form-check form-check-custom form-check-success form-check-solid my-2">
-                                                <input class="form-check-input" type="checkbox" value="" checked />
+                                                <input class="form-check-input" name="consentimento" type="checkbox" value="1" checked />
                                                 <label class="form-check-label" for="">
                                                     Consente o Tratamento Médico
                                                 </label>
                                             </div>
 
                                             <div class="form-check form-check-custom form-check-success form-check-solid my-2">
-                                                <input class="form-check-input" type="checkbox" value="" checked />
+                                                <input class="form-check-input" name="autorizacao" type="checkbox" value="1" checked />
                                                 <label class="form-check-label" for="">
                                                     Autoriza Partilha de Informação Médica
                                                 </label>
@@ -193,6 +239,102 @@ $page_name = "CareSys | SCLINICO - Consulta - " . $consultation_info["utente"];
     </div>
 
     <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/foo.php") ?>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const form = document.getElementById("form-consultation");
+            form.addEventListener("submit", doAppointment);
+
+            const api_url = "http://localhost:4001/api/";
+            const path = "consultations/consultas/";
+
+            function doAppointment() {
+                event.preventDefault();
+
+                var form = document.getElementById("form-consultation");
+
+                const formData = {
+                    hashed_id: '<?php echo $consultation_hashed_id ?>',
+                    motivo: form.motivo.value,
+                    problemas: form.problemas.value,
+                    obs_gerais: form.obs_gerais.value,
+                    recomendacoes: form.recomendacoes.value,
+                    tratamento: form.tratamento.value,
+                    progresso: form.progresso.value,
+                    diagnosticos_consulta: form.diagnostico.value,
+                    patologias_consulta: form.patologias.value,
+                    consentimento: form.consentimento.checked ? 1 : 0,
+                    autorizacao: form.autorizacao.checked ? 1 : 0,
+                };
+
+                // Get values form a select2 with mutiple select and convert to json
+                var diagnostico = $("#form-consultation select[name='diagnostico']").val();
+                var patologias = $("#form-consultation select[name='patologias']").val();
+
+                var diagnosticos_JSON = '';
+                var patologias_JSON = '';
+
+                // join the multiple diagnosticos with comma
+                if (diagnostico != null) {
+                    diagnosticos_JSON = diagnostico.join();
+                }
+                // join the multiple patologias with comma
+                if (patologias != null) {
+                    patologias_JSON = patologias.join();
+                }
+
+                formData.diagnosticos_consulta = '{'+ diagnosticos_JSON + '}';
+                formData.patologias_consulta = '{'+ patologias_JSON + '}';
+
+
+                fetch(api_url + path + "realizar", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(formData),
+                    })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        if (data.status) {
+                            Swal.fire({
+                                icon: "success",
+                                title: "Sucesso!",
+                                text: data.message,
+                                buttonsStyling: false,
+                                allowOutsideClick: false,
+                                didOpen: () => {
+                                    const confirmButton = Swal.getConfirmButton();
+                                    confirmButton.blur();
+                                },
+                                customClass: {
+                                    confirmButton: "btn fw-bold btn-primary",
+                                },
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.href = "lista";
+                                }
+                            });
+                        } else {
+                            console.log(JSON.stringify(formData)),
+                                Swal.fire({
+                                    icon: "error",
+                                    title: "Ocorreu um Erro!",
+                                    text: data.error,
+                                    confirmButtonText: "Voltar a Marcação",
+                                    buttonsStyling: false,
+                                    customClass: {
+                                        confirmButton: "btn btn-danger",
+                                    },
+                                });
+                        }
+                    })
+                    .catch((error) => {
+                        console.error("Error:", error);
+                    });
+            }
+        });
+    </script>
 
 </body>
 
