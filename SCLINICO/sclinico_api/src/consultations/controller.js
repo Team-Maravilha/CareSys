6c35555061572d0ac1ba6f09fc97580fcf5935e2
João Correia <j.correia2213@gmail.com>
2024-01-04 17:04:12 +0000

    Update RNU connect JMAI
    Update RNU connect JMAI

--- SCLINICO/sclinico_api/src/consultations/controller.js
+++ SCLINICO/sclinico_api/src/consultations/controller.js
@@ -1,5 +1,33 @@
 const pool = require("../../db");
 
+// Definir as tags da Documentação
+/**
+ * @swagger
+ * tags:
+ *   name: Informação
+ *   description: Gestão de Informação (Vacinas, Medicação, Exames, etc)
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Consultas
+ *   description: Gestão de Consultas
+ */
+
+/**
+* @swagger
+* /api/consultations/vacinas:
+*  get:
+*      tags: [Informação]
+*      summary: Ver Todas as Vacinas
+*      description: Retorna a Lista de Todas as Vacinas
+*      responses:
+*          '200':
+*              description: Sucesso
+*          '400':
+*              description: Erro
+*/
 const Get_All_Vacines = (req, res) => {
     pool.query("SELECT * FROM ver_vacinas()", (error, results) => {
         if (error) {
@@ -10,6 +38,19 @@ const Get_All_Vacines = (req, res) => {
     });
 }
 
+/**
+* @swagger
+* /api/consultations/patologias:
+*  get:
+*      tags: [Informação]
+*      summary: Ver Todas as Patologias
+*      description: Retorna a Lista de Todas as Patologias
+*      responses:
+*          '200':
+*              description: Sucesso
+*          '400':
+*              description: Erro
+*/
 const Get_All_Patologies = (req, res) => {
     pool.query("SELECT * FROM ver_patologias()", (error, results) => {
         if (error) {
@@ -20,6 +61,19 @@ const Get_All_Patologies = (req, res) => {
     });
 }
 
+/**
+ * @swagger
+ * /api/consultations/medicamentos:
+ *  get:
+ *     tags: [Informação]
+ *     summary: Ver Todos os Medicamentos
+ *     description: Retorna a Lista de Todos os Medicamentos
+ *     responses:
+ *      '200':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Get_All_Medications = (req, res) => {
     pool.query("SELECT * FROM ver_medicamentos()", (error, results) => {
         if (error) {
@@ -30,6 +84,19 @@ const Get_All_Medications = (req, res) => {
     });
 }
 
+/**
+ * @swagger
+ * /api/consultations/gabinetes:
+ *  get:
+ *     tags: [Informação]
+ *     summary: Ver Todos os Gabinetes
+ *     description: Retorna a Lista de Todos os Gabinetes
+ *     responses:
+ *      '200':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Get_All_Offices = (req, res) => {
     pool.query("SELECT * FROM ver_gabinetes()", (error, results) => {
         if (error) {
@@ -40,6 +107,19 @@ const Get_All_Offices = (req, res) => {
     });
 }
 
+/**
+ * @swagger
+ * /api/consultations/exames:
+ *  get:
+ *     tags: [Informação]
+ *     summary: Ver Todos os Exames
+ *     description: Retorna a Lista de Todos os Exames
+ *     responses:
+ *      '200':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Get_All_Exams = (req, res) => {
     pool.query("SELECT * FROM ver_exames()", (error, results) => {
         if (error) {
@@ -50,6 +130,19 @@ const Get_All_Exams = (req, res) => {
     });
 }
 
+/**
+ * @swagger
+ * /api/consultations/especialidades:
+ *  get:
+ *     tags: [Informação]
+ *     summary: Ver Todas as Especialidades
+ *     description: Retorna a Lista de Todas as Especialidades
+ *     responses:
+ *      '200':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Get_All_Specialities = (req, res) => {
     pool.query("SELECT * FROM ver_especialidades()", (error, results) => {
         if (error) {
@@ -60,6 +153,19 @@ const Get_All_Specialities = (req, res) => {
     });
 }
 
+/**
+ * @swagger
+ * /api/consultations/diagnosticos:
+ *  get:
+ *     tags: [Informação]
+ *     summary: Ver Todos os Diagnosticos
+ *     description: Retorna a Lista de Todos os Diagnosticos
+ *     responses:
+ *      '200':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Get_All_Diagnosis = (req, res) => {
     pool.query("SELECT * FROM ver_diagnosticos()", (error, results) => {
         if (error) {
@@ -70,6 +176,26 @@ const Get_All_Diagnosis = (req, res) => {
     });
 }
 
+/**
+ * @swagger
+ * /api/consultations/consulta/{hashed_id_consulta}:
+ *  get:
+ *     tags: [Consultas]
+ *     summary: Ver Consulta por ID
+ *     description: Retorna a Consulta com o ID especificado
+ *     parameters:
+ *      - in: path
+ *        name: hashed_id_consulta
+ *        required: true
+ *        description: ID da Consulta
+ *        schema:
+ *          type: string
+ *     responses:
+ *      '200':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Get_Consultation_By_Id = async (req, res) => {
     const axios = require('axios').default;
     const { hashed_id_consulta } = req.params;
@@ -106,7 +232,7 @@ const Get_Consultation_By_Id = async (req, res) => {
             res.status(400).json({ error: 'Erro ao carregar utentes' });
         }
         const utentes = patientsResponse.data.data;
-    
+
 
         // Map over consultas and update with additional information
         consultas = consultas.map((consulta) => {
@@ -138,6 +264,41 @@ const Get_Consultation_By_Id = async (req, res) => {
     }
 };
 
+/**
+ * @swagger
+ * /api/consultations/consultas:
+ *  post:
+ *     tags: [Consultas]
+ *     summary: Ver Consultas
+ *     description: Retorna a Lista de Consultas
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              hashed_id:
+ *                  type: string
+ *                  description: Hashed ID do Utente
+ *              status:
+ *                  type: string
+ *                  description: Status da Consulta
+ *              data_inicio:
+ *                  type: string
+ *                  description: Data de Inicio da Consulta
+ *              id_medico:
+ *                  type: integer
+ *                  description: ID do Médico
+ *              hashed_id_consulta:
+ *                  type: string
+ *                  description: Hashed ID da Consulta
+ *     responses:
+ *      '200':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Get_All_Consultations = async (req, res) => {
     const axios = require('axios').default;
     const { hashed_id, status, data_inicio, id_medico, hashed_id_consulta } = req.body;
@@ -175,7 +336,7 @@ const Get_All_Consultations = async (req, res) => {
             res.status(400).json({ error: 'Erro ao carregar utentes' });
         }
         const utentes = patientsResponse.data.data;
-    
+
 
         // Map over consultas and update with additional information
         consultas = consultas.map((consulta) => {
@@ -207,7 +368,41 @@ const Get_All_Consultations = async (req, res) => {
     }
 };
 
-
+/**
+ * @swagger
+ * /api/consultations/consultas/table:
+ *  post:
+ *     tags: [Consultas]
+ *     summary: Ver Consultas para DataTable
+ *     description: Retorna a Lista de Consultas para DataTable
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              hashed_id:
+ *                  type: string
+ *                  description: Hashed ID do Utente
+ *              status:
+ *                  type: string
+ *                  description: Status da Consulta
+ *              data_inicio:
+ *                  type: string
+ *                  description: Data de Inicio da Consulta
+ *              id_medico:
+ *                  type: integer
+ *                  description: ID do Médico
+ *              hashed_id_consulta:
+ *                  type: string
+ *                  description: Hashed ID da Consulta
+ *     responses:
+ *      '200':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Get_All_Consultations_for_DataTable = async (req, res) => {
     const axios = require('axios').default;
     const { hashed_id, status, data_inicio, id_medico, hashed_id_consulta } = req.body;
@@ -245,7 +440,7 @@ const Get_All_Consultations_for_DataTable = async (req, res) => {
             res.status(400).json({ error: 'Erro ao carregar utentes' });
         }
         const utentes = patientsResponse.data.data;
-    
+
 
         // Map over consultas and update with additional information
         consultas = consultas.map((consulta) => {
@@ -277,6 +472,50 @@ const Get_All_Consultations_for_DataTable = async (req, res) => {
     }
 }
 
+/**
+ * @swagger
+ * /api/consultations/consultas/adicionar:
+ *  post:
+ *     tags: [Consultas]
+ *     summary: Adicionar Consulta
+ *     description: Adiciona uma Consulta
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              id_utente:
+ *                  type: integer
+ *                  description: ID do Utente
+ *              id_unidade_saude:
+ *                  type: integer
+ *                  description: ID da Unidade de Saúde
+ *              id_gabinete:
+ *                  type: integer
+ *                  description: ID do Gabinete
+ *              id_especialidade:
+ *                  type: integer
+ *                  description: ID da Especialidade
+ *              id_medico:
+ *                  type: integer
+ *                  description: ID do Médico
+ *              data_inicio:
+ *                  type: string
+ *                  description: Data de Inicio da Consulta
+ *              hora_inicio:
+ *                  type: string
+ *                  description: Hora de Inicio da Consulta
+ *              tipo_consulta:
+ *                  type: string
+ *                  description: Tipo de Consulta
+ *     responses:
+ *      '201':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Add_Consultation = (req, res) => {
     const { id_utente, id_unidade_saude, id_gabinete, id_especialidade, id_medico, data_inicio, hora_inicio, tipo_consulta } = req.body;
 
@@ -290,6 +529,59 @@ const Add_Consultation = (req, res) => {
     });
 }
 
+/**
+ * @swagger
+ * /api/consultations/consultas/realizar:
+ *  post:
+ *     tags: [Consultas]
+ *     summary: Realizar Consulta
+ *     description: Realiza uma Consulta
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              hashed_id:
+ *                  type: string
+ *                  description: Hashed ID da Consulta
+ *              problemas:
+ *                  type: string
+ *                  description: Problemas
+ *              obs_gerais:
+ *                  type: string
+ *                  description: Observações Gerais
+ *              recomendacoes:
+ *                  type: string
+ *                  description: Recomendações
+ *              tratamento:
+ *                  type: string
+ *                  description: Tratamento
+ *              progresso:
+ *                  type: string
+ *                  description: Progresso
+ *              consentimento:
+ *                  type: boolean
+ *                  description: Consentimento
+ *              autorizacao:
+ *                  type: boolean
+ *                  description: Autorização
+ *              diagnosticos_consulta:
+ *                  type: string
+ *                  description: Diagnósticos da Consulta
+ *              patologias_consulta:
+ *                  type: string
+ *                  description: Patologias da Consulta
+ *              motivo:
+ *                  type: string
+ *                  description: Motivo da Consulta
+ *     responses:
+ *      '201':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Do_Consultation = (req, res) => {
     const { hashed_id, problemas, obs_gerais, recomendacoes, tratamento, progresso, consentimento, autorizacao, diagnosticos_consulta, patologias_consulta, motivo } = req.body;
 
@@ -303,6 +595,29 @@ const Do_Consultation = (req, res) => {
     });
 }
 
+/**
+ * @swagger
+ * /api/consultations/consultas/cancelar:
+ *  post:
+ *     tags: [Consultas]
+ *     summary: Cancelar Consulta
+ *     description: Cancela uma Consulta
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              hashed_id:
+ *                  type: string
+ *                  description: Hashed ID da Consulta
+ *     responses:
+ *      '201':
+ *          description: Sucesso
+ *      '400':
+ *          description: Erro
+*/
 const Cancel_Consultation = (req, res) => {
     const { hashed_id } = req.body;
 
@@ -315,12 +630,6 @@ const Cancel_Consultation = (req, res) => {
         res.status(201).json({ status: true, message: "Consulta cancelada com sucesso!" })
     });
 }
-
-
-
-
-
-
 
 
 module.exports = {
